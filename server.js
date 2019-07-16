9f7fade5fc05f71b5fa4aa743f8d0514603d7dee
Robert Kesterson <rkesters@gmail.com>
2019-07-16 01:19:11 -0400

    init

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
-var api = require('./api/api');
 var express = require('express');
 var path = require('path');
 var morgan = require('morgan');
 
 var argv = require('yargs')
-            .usage('Start the server')
-            .default('port', process.env.WEB_PORT || 1337)
-            .alias('port', 'p')
-            .describe('port', 'port to run on')
-            .argv;
+  .usage('Start the server')
+  .default('port', process.env.WEB_PORT || 1337)
+  .alias('port', 'p')
+  .describe('port', 'port to run on').argv;
 
 var app = express();
-app.use(express.static(path.join(__dirname, "/dist")));
-app.use(api);
+app.use(express.static(path.join(__dirname, '/dist')));
 
-console.log("Now listening on port " + argv.port);
-console.log("Connect to http://0.0.0.0:" + argv.port);
+console.log('Now listening on port ' + argv.port);
+console.log('Connect to http://0.0.0.0:' + argv.port);
 app.listen(argv.port);
